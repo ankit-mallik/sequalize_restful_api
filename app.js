a5e122387db4232a37fb86375ae147d4c206edfc
ankit-mallik <hritikmallik@gmail.com>
2025-01-13 23:47:17 +0545

    Added signup with bcrypt encryption

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config({path: `${process.cwd()}/.env`});
+const express = require('express');
+
+
+
+const authRouter = require('./route/authRoute')
+
+const app = express();
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        message: 'REST API are working',
+    })
+})
+
+// all routes will be here
+
+app.use('/api/v1/auth', authRouter);
+
+
+app.use('*',(req,res,next)=>{
+    res.status(404).json({
+        status: 'fail',
+        message: 'Route not found!!'
+    })
+})
+
+
+const PORT = process.env.APP_PORT || 4000;
+app.listen(PORT,()=>{
+    console.log(`Server up and running`, PORT);
+})
